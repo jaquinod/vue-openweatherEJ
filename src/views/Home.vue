df7fdac1e90fe549a8ded78a329ae0ba4beea8f4
Eddy <eddy.jaquinod@gmail.com>
2021-10-13 15:54:56 +0200

    essay7

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <b-container :fluid="true" class="h-100">
     <b-sidebar
       id="sidebar-variant"
-      title="Recherche de villeEJ6"
+      title="Recherche de villeEJ7"
       bg-variant="dark"
       text-variant="light"
       shadow
